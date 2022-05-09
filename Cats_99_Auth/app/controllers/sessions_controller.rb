b9a7e3498c5fd5f80239f18a7d01fbc06e0fb4db
Kevin <NateraKevin1@gmail.com>
2022-05-09 16:44:30 -0400

    completed phase 4

--- Cats_99_Auth/app/controllers/sessions_controller.rb
+++ Cats_99_Auth/app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
-
+    before_action :require_logged_out, only: [:new, :create]
+    before_action :require_logged_in, only: [:index, :show, :edit, :update]
     def new
         @user = User.new 
         render :new 
-
     end
 
 
@@ -15,17 +15,18 @@ class SessionsController < ApplicationController
 
         )
 
-
         if @user 
-            #login
+            login(@user)
             session[:session_token] = @user.reset_session_token!
             redirect_to cats_url
         else  
             render :new
         end
-
     end
 
-
+    def destroy 
+        logout!
+        redirect_to new_session_url
+    end
 
 end
--- Cats_99_Auth/app/controllers/users_controller.rb
+++ Cats_99_Auth/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+    before_action :require_logged_out, only: [:new, :create]
+    before_action :require_logged_in, only: [:index, :show, :edit, :update]
     def new
         @user = User.new 
         render :new
@@ -9,7 +10,7 @@ class UsersController < ApplicationController
     def create 
         @user = User.new(user_params)
         if @user.save 
-            #login
+            login(@user)
             redirect_to cats_url 
         else  
             render json: @user.errors.full_messages, status: 422
