dd67be1020e6dc3ec4b6b4ee0a5220b9b05b078a
Kevin <NateraKevin1@gmail.com>
2022-05-09 14:50:18 -0400

    added ensure_session_token method

--- Cats_99_Auth/app/models/user.rb
+++ Cats_99_Auth/app/models/user.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  user_name       :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+    validates :user_name, :password_digest, :session_token, presence: true
+    validates :user_name, :session_token, uniqueness:true
+    
+    before_validation :ensure_session_token
+
+    
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
 end
