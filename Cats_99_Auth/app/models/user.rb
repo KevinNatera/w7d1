91730035e74080be3af9eb6d792399653d6fc96e
Kevin <NateraKevin1@gmail.com>
2022-05-09 14:51:50 -0400

    added reset_session_token method

--- Cats_99_Auth/app/models/user.rb
+++ Cats_99_Auth/app/models/user.rb
@@ -15,8 +15,15 @@ class User < ApplicationRecord
     
     before_validation :ensure_session_token
 
-    
+
     def ensure_session_token
         self.session_token ||= SecureRandom::urlsafe_base64
     end
+
+    def reset_session_token!
+        self.sessiontoken = SecureRandom::urlsafe_base64
+        self.save! 
+        self.session_token
+    end
+    
 end
