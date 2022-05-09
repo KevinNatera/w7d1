b99b93f282702aa5ca830773b7b9093dc1312525
Kevin <NateraKevin1@gmail.com>
2022-05-09 15:49:00 -0400

    .

--- Cats_99_Auth/app/controllers/users_controller.rb
+++ Cats_99_Auth/app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save 
             #login
-            redirect_to user_url(@user.id)
+            redirect_to cats_url 
         else  
             render json: @user.errors.full_messages, status: 422
         end
